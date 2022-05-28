8a598378c51cf518a1538127050f5ca8d1d34cf8
André Pereira <andrepereira97@yahoo.com>
2022-05-28 20:25:35 -0300

    :sparkles: add border-previewer project

---
+++ border-previewer-vue/README.md
@@ -0,0 +1,47 @@
+# Border previewer
+
+## About
+
+I already have a project for [border-radius previewer](https://github.com/dre1597/border-radius-previewer) using plain HTML/CSS. The idea for this project is to make more border options (to keep the old project just with border-radius) and practice Vue.
+
+This project is from this [idea](https://github.com/florinpop17/app-ideas/blob/master/Projects/1-Beginner/Bin2Dec-App.md)
+
+## Functionalities to add
+
+-   Border-radius preview
+-   More options to border like width, style, color
+-   Option to copy to clipboard
+-   Option to change the background color and text color of the page
+
+## Technologies
+
+-   Vue
+-   SASS
+
+## Project setup
+
+```
+yarn install
+```
+
+### Compiles and hot-reloads for development
+
+```
+yarn serve
+```
+
+### Compiles and minifies for production
+
+```
+yarn build
+```
+
+### Lints and fixes files
+
+```
+yarn lint
+```
+
+### Customize configuration
+
+See [Configuration Reference](https://cli.vuejs.org/config/).
---
+++ border-previewer-vue/src/App.vue
@@ -0,0 +1,111 @@
+<template>
+    <div id="app">
+        <h1 class="title">{{ title }}</h1>
+        <form action=""></form>
+        <form action="">
+            <fieldset :key="info" v-for="info in fieldsInfos">
+                <label :for="info.name">{{ info.label }}</label>
+                <input type="text" :id="info.name" />
+                <select name="units" id="units">
+                    <option
+                        :key="option"
+                        v-for="option in options"
+                        :value="option.value"
+                        >{{ option.label }}</option
+                    >
+                </select>
+            </fieldset>
+            <button type="button">Generate</button>
+        </form>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'App',
+        data() {
+            return {
+                title: 'Border previewer',
+                fieldsInfos: [
+                    {
+                        label: 'Top Left',
+                        name: 'top-left',
+                    },
+                    {
+                        label: 'Top Right',
+                        name: 'top-right',
+                    },
+                    {
+                        label: 'Bottom Left',
+                        name: 'bottom-left',
+                    },
+                    {
+                        label: 'Bottom Right',
+                        name: 'bottom-right',
+                    },
+                ],
+                options: [
+                    {
+                        label: 'px',
+                        value: 'pixel',
+                    },
+                    {
+                        label: '%',
+                        value: 'percent',
+                    },
+                ],
+            };
+        },
+    };
+</script>
+
+<style lang="scss">
+    * {
+        padding: 0;
+        margin: 0;
+        box-sizing: border-box;
+    }
+    #app {
+        text-align: center;
+        color: #42b883;
+        background-color: #2c3e50;
+        height: 100vh;
+    }
+    .title {
+        padding-top: 60px;
+    }
+    fieldset {
+        border: none;
+        padding: 16px;
+    }
+    fieldset label {
+        padding: 16px;
+    }
+    fieldset input {
+        padding: 5px;
+        margin-right: 5px;
+        width: 100px;
+    }
+    button {
+        margin-top: 20px;
+        padding: 16px;
+        font-size: 20px;
+        width: 200px;
+        border: none;
+        outline: none;
+        background-color: #42b883;
+        color: #fff;
+        cursor: pointer;
+    }
+    select {
+        padding: 5px;
+        background-color: transparent;
+        color: #fff;
+        border: none;
+        outline: none;
+    }
+    option {
+        background-color: #2c3e50;
+        color: #fff;
+    }
+</style>
